9f9a909e6493469d6598c727bf8cccf83384ff8c
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-22 19:10:31 +0200

    feat: file function.php importandoci la logica

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php 
-        if (isset($_POST["email"])) {
-            $user_email = $_POST["email"];
-            if (strpos($user_email, "@") !== false && strpos($user_email, ".") !== false) {
-                $return= "Formato mail corretto";
-            } else {
-                $return= "Devi inserire la @ o il .";
-            }
-        }
-        ?>
+<?php require_once __DIR__ . "/partials/function.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
