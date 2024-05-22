2fac3d7901f99aca1c654c882bdf3b8528e37af2
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-22 15:20:24 +0200

    feat: form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,13 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
-    
+    <form class="p-5 d-flex" action="">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        <input type="email" class="form-control w-25 mx-1" id="exampleFormControlInput1" placeholder="user@gmail.com">
+        <button type="button" class="btn btn-primary px-3">Invia</button>
+    </form>
 </body>
+
 </html>
